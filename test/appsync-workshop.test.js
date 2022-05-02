d0aa3c4279255d105e76ed67769b34542cd50660
Neil Brown <neil.brown.git@gmail.com>
2022-05-02 12:39:04 +0100

    Initial commit

---
+++ test/appsync-workshop.test.js
@@ -0,0 +1,18 @@
+// const { Template } = require('@aws-cdk/assertions');
+// const cdk = require('@aws-cdk/core');
+// const AppsyncWorkshop = require('../lib/appsync-workshop-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/appsync-workshop-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new AppsyncWorkshop.AppsyncWorkshopStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
+
