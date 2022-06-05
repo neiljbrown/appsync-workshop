fa85f5e939664cd92dda67d54881bf9529c2a197
Neil Brown <neil.brown.git@gmail.com>
2022-06-05 17:29:52 +0100

    Add web-app that uses Appsync GraphQL API to display data-points in web UI, in real time.

---
+++ application/src/aws-exports.js
@@ -0,0 +1,7 @@
+const awsmobile = {
+  aws_appsync_graphqlEndpoint: 'https://<insert-subdomain-for-your-api>.appsync-api.eu-west-1.amazonaws.com/graphql',
+  aws_appsync_apiKey: '<insert-api-key-for-your-api>',
+  aws_appsync_authenticationType: 'API_KEY',
+}
+
+export default awsmobile
