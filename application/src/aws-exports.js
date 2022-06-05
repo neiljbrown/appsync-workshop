ba6db2cf1a5dad6af652c70671f482ec1a01acfd
Neil Brown <neil.brown.git@gmail.com>
2022-06-05 17:51:33 +0100

    Reconfigure method that web-app uses to authenticate/authorise its GraphQL API requests - changing it from an API key (only suitable for public APIs) to delegated user authorisation (OAuth) supported by a configured Amazon Cognito User Pool.

--- application/src/aws-exports.js
+++ application/src/aws-exports.js
@@ -1,7 +1,10 @@
 const awsmobile = {
-  aws_appsync_graphqlEndpoint: 'https://<insert-subdomain-for-your-api>.appsync-api.eu-west-1.amazonaws.com/graphql',
-  aws_appsync_apiKey: '<insert-api-key-for-your-api>',
-  aws_appsync_authenticationType: 'API_KEY',
+  aws_appsync_graphqlEndpoint: 'https://<your-appsync-api-subdomain>.appsync-api.eu-west-1.amazonaws.com/graphql',
+  aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
+  aws_appsync_region: 'eu-west-1',
+  aws_cognito_region: 'eu-west-1',
+  aws_user_pools_id: '<your-user-pool-id>',
+  aws_user_pools_web_client_id: '<your-user-pool-web-client-id>',
 }
 
 export default awsmobile
