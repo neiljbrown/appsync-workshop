ad16a2394c5a7c71063c499f1322d781d2dda51a
Neil Brown <neil.brown.git@gmail.com>
2023-01-03 18:17:17 +0000

    Replace all region and app specific values in the Amplify web-app's AWS service config with token values, and polish existing token names.

--- application/src/aws-exports.js
+++ application/src/aws-exports.js
@@ -1,10 +1,10 @@
 const awsmobile = {
-  aws_appsync_graphqlEndpoint: 'https://<your-appsync-api-subdomain>.appsync-api.eu-west-1.amazonaws.com/graphql',
+  aws_appsync_graphqlEndpoint: 'https://<your-appsync-api-subdomain>.appsync-api.<your-appsync-aws-region>.amazonaws.com/graphql',
   aws_appsync_authenticationType: 'AMAZON_COGNITO_USER_POOLS',
-  aws_appsync_region: 'eu-west-1',
-  aws_cognito_region: 'eu-west-1',
-  aws_user_pools_id: '<your-user-pool-id>',
-  aws_user_pools_web_client_id: '<your-user-pool-web-client-id>',
+  aws_appsync_region: '<your-appsync-aws-region>',
+  aws_cognito_region: '<your-cognito-aws-region>',
+  aws_user_pools_id: '<your-cognito-user-pool-id>',
+  aws_user_pools_web_client_id: '<your-cognito-user-pool-web-client-id>',
 }
 
 export default awsmobile
